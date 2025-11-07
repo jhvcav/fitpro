6f8eb83efd416df29d4b51ee00d370743c51a830
jhvcav <156108984+jhvcav@users.noreply.github.com>
2025-11-07 18:23:37 +0000

    MAJ

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,6 @@ export default function App() {
             {activeSection === 'home' && (
               <CTASection setActiveSection={setActiveSection} />
             )}
-
             {activeSection === 'offres' && (
               <OffersSection
                 setActiveSection={setActiveSection}
